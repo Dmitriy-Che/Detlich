b9acd7bfc5152aa9737b44492bb414bd6ba640a0
Dmitriy-Che <81006353+Dmitriy-Che@users.noreply.github.com>
2025-12-17 14:49:29 +0300

    1449

---
+++ index.js
@@ -0,0 +1,19 @@
+
+import '@angular/compiler';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { NgZone, ɵNoopNgZone } from '@angular/core';
+
+import { AppComponent } from './src/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    // Manually provide NoopNgZone to run the application without Zone.js
+    { provide: NgZone, useClass: ɵNoopNgZone },
+    provideHttpClient(),
+  ],
+}).catch(err => console.error(err));
+
+// This file was renamed from index.tsx to index.js for compatibility
+// with static hosting services like GitHub Pages, which require a .js
+// extension to serve the file as an executable JavaScript module.
